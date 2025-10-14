f6338b18852b2175d86799f66abb56685cef2245
alljju <cyjj625@naver.com>
2025-10-14 20:33:15 +0900

    cdcd

--- src/main/java/com/skala/springbootsample/dto/BookingRequestDto.java
+++ src/main/java/com/skala/springbootsample/dto/BookingRequestDto.java
@@ -1,14 +1,23 @@
 package com.skala.springbootsample.dto;
 
-import java.time.LocalDateTime;
-
 public class BookingRequestDto {
-    private Long parkingId;
-    private LocalDateTime startDateTime;
-    private LocalDateTime endDateTime;
-    private VehicleDto vehicle;
-    private UserDto user;
-    private PaymentDto payment;
+    private String bookingId;
+    private String customerName;
 
-    // Getters and Setters
+    // Getter, Setter
+    public String getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(String bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 }
